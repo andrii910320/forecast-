f08c1ad2cf3b6974c73a4003171a7fce2449e772
andrii910320 <andrii.ryzhuk98@gmail.com>
2024-02-18 18:37:15 +0200

    save

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 import LoginWithGoogle from "@/components/UI/LoginWithGoogle";
-import Image from "next/image";
-// import { useGoogleLogin } from "@react-oauth/google";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import s from "./style.module.scss";
 
 export default function Login() {
   return (
     <main className={s.mainPage}>
       <h1 className={s.title}> FORECAST</h1>
-      {/* <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID}>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID}>
         <LoginWithGoogle />
-      </GoogleOAuthProvider> */}
+      </GoogleOAuthProvider>
     </main>
   );
 }
